82dcc5ab272a1415bcf7db275e2e0ab62276f6b2
Edgars Skrabins <shiftkiro@gmail.com>
2024-01-15 20:42:24 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {Piano} from "./components/Piano/Piano.tsx";
+
+function App() {
+
+    return (
+        <>
+            <Piano/>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/Piano/Piano.tsx
@@ -0,0 +1,58 @@
+import { useEffect } from 'react';
+import piano1 from "./1.ogg"
+
+const PianoApp = () => {
+
+    useEffect(() => {
+        const initializeMIDI = async () => {
+            try {
+                if (navigator.requestMIDIAccess) {
+                    const midiAccess = await navigator.requestMIDIAccess();
+                    const inputs = midiAccess.inputs.values();
+
+                    const inputArr = Array.from(inputs);
+
+                    inputArr.forEach((input) => {
+                        input.onmidimessage = (event) => {
+                            const [command, note, velocity] = (event as MIDIMessageEvent).data;
+                            if (command === 144 && velocity !== 0) {
+                                playSound(note);
+                            } else if (command === 128 || (command === 144 && velocity === 0)) {
+                                stopSound(note);
+                            }
+                        }
+                    })
+                } else {
+                    console.log('Web MIDI API not supported');
+                }
+            } catch (error) {
+                console.error('Error initializing MIDI:', error);
+            }
+        };
+
+        initializeMIDI();
+
+
+    }, []);
+
+    const initializeSounds = () => {
+
+    }
+
+    const playSound = (note:number) => {
+        const testSound = new Audio(piano1);
+        testSound.play();
+    };
+
+    const stopSound = (note) => {
+        // Implement logic to stop the sound when the key is released
+    };
+
+    return (
+        <div className="piano-keys">
+            {/* Render piano keys as needed */}
+        </div>
+    );
+};
+
+export default PianoApp;
